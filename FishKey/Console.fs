b2cd7a8c4da2c87f9dda7c014cbf6167c0ed73b3
FatherFoxxy <rattusunison@gmail.com>
2019-08-07 19:13:33 -0700

    Internalize PInvoke calls

--- FishKey/Console.fs
+++ FishKey/Console.fs
@@ -1,17 +1,20 @@
 [<RequireQualifiedAccess>]
 module Console
-open System.Runtime.InteropServices
-[<DllImport("user32.dll")>]
-extern bool ShowWindow(nativeint hWnd, int flags)
 
-[<DllImport("kernel32.dll")>]
-extern nativeint GetConsoleWindow()
+module internal PInvoke =
+    open System.Runtime.InteropServices
+    [<DllImport("user32.dll")>]
+    extern bool ShowWindow(nativeint hWnd, int flags)
+
+    [<DllImport("kernel32.dll")>]
+    extern nativeint GetConsoleWindow()
+open PInvoke
 
 let private showWindowWithFlags flags =
     ShowWindow(GetConsoleWindow(), flags)
 
-let Hide() = 
+let Hide() =
     ShowWindow(GetConsoleWindow(), 0)
 
-let Show() = 
+let Show() =
     ShowWindow(GetConsoleWindow(), 9)
